d5cfb6fcfb30dec361a01c04169e2934e4a6324a
valesuarez <v.suarez190687@gmail.com>
2024-03-18 12:38:59 -0300

    primer tarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContenidoComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contenido/contenido.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contenido',
+  templateUrl: './contenido.component.html',
+  styleUrl: './contenido.component.scss'
+})
+export class ContenidoComponent {
+
+}
